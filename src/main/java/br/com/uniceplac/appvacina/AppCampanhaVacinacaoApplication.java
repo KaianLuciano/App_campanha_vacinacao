4469d9e5b6f7e8ec843922ee9313adb3c29d4266
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2023-05-24 20:14:00 -0300

    Inicializando Projeto

---
+++ src/main/java/br/com/uniceplac/appvacina/AppCampanhaVacinacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniceplac.appvacina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCampanhaVacinacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCampanhaVacinacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uniceplac/appvacina/AppCampanhaVacinacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniceplac.appvacina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCampanhaVacinacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
