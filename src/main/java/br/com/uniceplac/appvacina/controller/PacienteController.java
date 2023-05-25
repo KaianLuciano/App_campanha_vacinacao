de28ccfbf4f966bc424fe819fbe7213c8e066fce
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2023-05-24 22:27:58 -0300

    Inserido CRUD Paciente

--- src/main/java/br/com/uniceplac/appvacina/controller/PacienteController.java
+++ src/main/java/br/com/uniceplac/appvacina/controller/PacienteController.java
@@ -1,9 +1,16 @@
 package br.com.uniceplac.appvacina.controller;
 
+import br.com.uniceplac.appvacina.DTO.CampanhaVacinacaoDTO;
+import br.com.uniceplac.appvacina.DTO.PacienteDTO;
+import br.com.uniceplac.appvacina.models.CampanhaVacinacaoModel;
+import br.com.uniceplac.appvacina.models.PacienteModel;
 import br.com.uniceplac.appvacina.service.PacienteService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pacientes")
@@ -11,4 +18,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
 
     final PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<Object> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.findAll());
+    }
+
+    @GetMapping("/{idPaciente}")
+    public ResponseEntity<Object> findById(@PathVariable(value = "idPaciente") String idPaciente) {
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.findById(idPaciente));
+    }
+    @PostMapping
+    public ResponseEntity<Object> savePaciente(@RequestBody PacienteModel pacienteModel) {
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.savePaciente(pacienteModel));
+    }
+
+    @PutMapping("/{idPaciente}")
+    public ResponseEntity<Object> updatePaciente(@PathVariable(value = "idPaciente") String idPaciente, @RequestBody PacienteModel pacienteModel) {
+        Optional<PacienteModel> pacienteModelOptional = pacienteService.findByIdPrivate(idPaciente);
+
+        if (pacienteModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente com o Id especificado não foi encontrado");
+        }
+
+        PacienteModel pacientePut = pacienteModelOptional.get();
+        pacientePut.setNome(pacienteModel.getNome());
+        pacientePut.setIdade(pacienteModel.getIdade());
+        pacientePut.setGenero(pacienteModel.getGenero());
+
+        pacienteService.savePaciente(pacientePut);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Paciente Atualizado: " + new PacienteDTO(pacientePut));
+    }
+
+    @DeleteMapping("/{idPaciente}")
+    public ResponseEntity<Object> deleteCampanha(@PathVariable(value = "idPaciente") String idPaciente) {
+        Optional<PacienteModel> pacienteModelOptional = pacienteService.findByIdPrivate(idPaciente);
+
+        if (pacienteModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente com o Id especificado não foi encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.deletePaciente(pacienteModelOptional.get()));
+    }
+
 }
--- src/main/java/br/com/uniceplac/appvacina/service/PacienteService.java
+++ src/main/java/br/com/uniceplac/appvacina/service/PacienteService.java
@@ -1,12 +1,42 @@
 package br.com.uniceplac.appvacina.service;
 
+import br.com.uniceplac.appvacina.DTO.CampanhaVacinacaoDTO;
+import br.com.uniceplac.appvacina.DTO.PacienteDTO;
+import br.com.uniceplac.appvacina.models.CampanhaVacinacaoModel;
+import br.com.uniceplac.appvacina.models.PacienteModel;
 import br.com.uniceplac.appvacina.repository.PacienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class PacienteService {
 
     final PacienteRepository pacienteRepository;
+
+    public List<PacienteDTO> findAll() {
+        List<PacienteModel> pacienteModels = pacienteRepository.findAll();
+        return pacienteModels.stream().map(x -> new PacienteDTO(x)).toList();
+    }
+
+    public PacienteDTO findById(String idPaciente) {
+        return new PacienteDTO(pacienteRepository.findById(idPaciente).get());
+    }
+
+    public Optional<PacienteModel> findByIdPrivate(String idPaciente) {
+        return pacienteRepository.findById(idPaciente);
+    }
+
+    public PacienteDTO savePaciente(PacienteModel pacienteModel) {
+        pacienteRepository.save(pacienteModel);
+        return new PacienteDTO(pacienteModel);
+    }
+
+    public PacienteDTO deletePaciente(PacienteModel pacienteModel) {
+        pacienteRepository.delete(pacienteModel);
+        return new PacienteDTO(pacienteModel);
+    }
 }
